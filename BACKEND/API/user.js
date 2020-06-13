c1d35d69e4942d6fe3245dbc84bf7113a82c01fb
anderson <anderdavid86@hotmail.com>
2020-06-12 21:56:54 -0500

    Se implemeta getId put y delete en api rest

--- BACKEND/API/user.js
+++ BACKEND/API/user.js
@@ -2,7 +2,8 @@ const pool = require('../db/database')
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port =3001
+const router = express.Router();
+const port =3003
 
 app.use(cors())
 
@@ -22,6 +23,21 @@ app.get('/user',(req,res)=>{
         res.send(JSON.stringify(response))
     })
 })
+app.get('/user/:id',(req,res)=>{
+
+    let query="SELECT *FROM user WHERE id ="+req.params.id
+    pool.query(query,(err,result,fields)=>{
+        if(err) throw err
+        response={
+            status:true,
+            user:result[0]
+        }
+        res.send(JSON.stringify(response))
+    })
+
+  
+})
+
 app.use(express.json())
 app.post('/user',(req,res)=>{
 
@@ -39,6 +55,42 @@ app.post('/user',(req,res)=>{
             status:true,
             result: result,
             msg:'usuario guardado',
+            query:query
+        }
+        res.send(JSON.stringify(response))
+    })
+})
+
+app.put('/user/:id',(req,res)=>{
+    let nombre = req.body.nombre
+	let apellido = req.body.apellido
+	let cedula = req.body.cedula
+	let edad = req.body.edad
+	let email = req.body.email
+    let password = req.body.password
+
+    let query ="UPDATE user u SET u.nombre='"+nombre+"',u.apellido= '"+apellido+"',u.cedula ='"+cedula+"',u.edad='"+edad+"',u.email='"+email+"',u.password='"+password+"' WHERE id ="+req.params.id
+    pool.query(query,(err,result,fields)=>{
+        if(err) throw err
+        response ={
+            status:true,
+            result: result,
+            msg:'usuario actualizado',
+            query:query
+        }
+        res.send(JSON.stringify(response))
+    })
+    
+})
+
+app.delete('/user/:id',(req,res)=>{
+    let query ="DELETE FROM user WHERE id="+req.params.id
+    pool.query(query,(err,result,fields)=>{
+        if(err) throw err
+        response ={
+            status:true,
+            result: result,
+            msg:'usuario borrado',
             query:query
         }
         res.send(JSON.stringify(response))
